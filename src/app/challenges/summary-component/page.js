bbefca1d42b05478be680e380333585dc499852d
k4ry32 <karinavrodriguez1609@gmail.com>
2023-09-14 20:05:58 -0300

    summary-component finished

--- src/app/challenges/summary-component/page.js
+++ src/app/challenges/summary-component/page.js
@@ -1,23 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Hanken_Grotesk } from 'next/font/google';
+import { data } from './data.json';
+import { IoMdArrowRoundBack } from 'react-icons/io';
+
+const hankenGrotesk = Hanken_Grotesk({ subsets: ['latin'] });
+
 const SummaryComponent = () => {
+
+    const colors = [
+        { bg: 'flex flex-row w-full p-3.5 rounded-xl mb-4 bg-[#ffebeb] justify-between', text: 'ml-2 lg:text-lg text-base font-semibold text-[#ff5757]' },
+        { bg: 'flex flex-row w-full p-3.5 rounded-xl mb-4 bg-[#fff4e0] justify-between', text: 'ml-2 lg:text-lg text-base font-semibold text-[#ffb01f]' },
+        { bg: 'flex flex-row w-full p-3.5 rounded-xl mb-4 bg-[#e0faf4] justify-between', text: 'ml-2 lg:text-lg text-base font-semibold text-[#00bd91]' },
+        { bg: 'flex flex-row w-full p-3.5 rounded-xl mb-4 bg-[#e5e7fb] justify-between', text: 'ml-2 lg:text-lg text-base font-semibold text-[#1125d4]' },
+    ]
+
     return (
-        <div className="min-h-screen flex place-content-center place-items-center p-20">
-            <div className="w-5/6 h-3/4 flex place-content-center bg-slate-200 rounded-xl p-4 shadow-xl">
-                <div className="w-3/5 h-full flex lg:flex-row sm:flex-col m-2 rounded-3xl bg-[#ffffff]">
-                    <div className="flex flex-col items-center w-1/2 bg-gradient-to-b from-[#7857ff] to-[#2e2be9] rounded-3xl text-center">
-                        <p className="text-xl mt-8 mb-5"> Your Result </p>
-                        <div className="w-32 h-32 p-7 rounded-full bg-gradient-to-b from-[#2e2be9] to-[#7857ff] text-center"
-                            style={{background: "linear-gradient(180deg, hsla(256, 72%, 46%, 1), hsla(241, 72%, 46%, 0) 90%);"}}
+        <div className={`min-h-screen flex flex-col place-items-center p-5 ${hankenGrotesk.className}`}>
+            <Link href={'/'} className="flex flex-row self-start items-center">
+                <IoMdArrowRoundBack className="text-xl"/>
+                <p className="text-xl ml-2"> Volver </p>
+            </Link>
+            <div className="flex place-content-center bg-slate-100 sm:rounded-xl sm:p-16 shadow-xl my-auto">
+                <div className="lg:w-[740px] lg:h-[510px] w-[380px] h-[800px] flex lg:flex-row flex-col lg:rounded-3xl bg-[#ffffff] shadow-xl">
+                    <div className="flex flex-col items-center lg:w-1/2 w-full bg-gradient-to-b from-[#7857ff] to-[#2e2be9] lg:rounded-3xl rounded-b-3xl text-center">
+                        <p className="lg:text-2xl text-lg font-bold lg:mt-8 lg:mb-10 mt-6 mb-6 text-[#c8c7ff]"> Your Result </p>
+                        <div className="flex flex-col place-content-center lg:w-48 lg:h-48 w-[8.5rem] h-[8.5rem] rounded-full bg-gradient-to-b from-[#2e2be9] to-[#7857ff] text-center"
+                            style={{background: "linear-gradient(180deg, hsla(256, 72%, 46%, 1), hsla(241, 72%, 46%, 0) 100%);"}}
                         >
-                            <p className="text-5xl"> 76 </p>
-                            <p> of 100 </p>
+                            <p className="lg:text-7xl text-[3.5rem] font-bold leading-tight"> 76 </p>
+                            <p className='lg:text-lg text-base lg:my-2 font-semibold text-slate-400'> of 100 </p>
                         </div>
-                        <p className="mt-6"> Great </p>
-                        <p className="mb-10 mt-3 mx-14">
+                        <p className="mt-6 lg:text-3xl text-2xl font-semibold"> Great </p>
+                        <p className="lg:mb-10 lg:mt-3 mb-9 mt-2 lg:mx-16 mx-12 lg:text-lg text-base text-[#c8c7ff]">
                             You scored higher than 65% of the people who have
                             taken these test
                         </p>
                     </div>
-                    <div></div>
+
+                    <div className='px-10 lg:w-1/2 w-full'>
+                        <p className="lg:text-2xl text-lg font-bold lg:mt-8 lg:mb-8 mt-6 mb-5 text-slate-900">Summary</p>
+                        {data.map((item, index) => {
+                            return (
+                                <div key={index} className={colors[index].bg}>
+                                    <div className="flex flex-row">
+                                        <Image src={item.icon} alt='icon' width={20} height={20} />
+                                        <p className={colors[index].text}> {item.category} </p>
+                                    </div>  
+                                    <div className="flex flex-row">
+                                        <p className="ml-2 lg:text-lg text-base font-bold text-[#303b5a]"> {item.score} </p>      
+                                        <p className="ml-2 lg:text-lg text-base font-bold text-slate-400"> / 100 </p>  
+                                    </div>                                                                    
+                                </div>
+                            )
+                        })}
+                        <button className="bg-[#303b5a] hover:bg-gradient-to-b hover:from-[#7857ff] hover:to-[#2e2be9] rounded-full w-full text-lg font-bold lg:mt-6 lg:mb-5 mt-2 lg:p-4 p-3.5 text-[#ebf1ff]">Continue</button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+    const links = [
+        {
+            name: 'Results summary component',
+            href: '/challenges/summary-component'
+        }
+    ];
+
     return (
-        <div className="flex min-h-screen flex-col items-center justify-between p-20">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-semibold text-lg lg:flex">
+        <div className="flex min-h-screen flex-col items-center justify-between lg:p-20 p-5">
+            <div className="flex z-10 lg:max-w-5xl w-full items-center justify-between font-semibold text-lg lg:flex mb-4">
                 <p className="text-2xl"> Frontend Challenges </p>
                 <p> 2023 </p>
             </div>
 
-            <div className="container max-w-4xl flex flex-row items-center justify-around bg-slate-900/50">
+            <div className="container lg:max-w-4xl flex lg:flex-row flex-col items-center justify-around bg-slate-900/50 rounded-md py-5 lg:py-0">
                 <Image
                     src="https://i.pinimg.com/originals/ca/75/3f/ca753f2cd3e13792e5a409d71557d41c.jpg"
                     width={250}
@@ -16,7 +24,7 @@ export default function Home() {
                     alt="baby-yoda"
                     className="rounded-lg"
                 />
-                <div className="max-w-lg p-4 leading-relaxed">
+                <div className="max-w-lg px-4 pt-4 lg:p-4 leading-relaxed">
                     <h1 className="text-xl mb-3"> Hola amigos! </h1>
                     <p className="mb-3">
                         Me llamo Karina Rodriguez y soy una desarrolladora
@@ -42,8 +50,17 @@ export default function Home() {
                 </div>
             </div>
 
-            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-                <p> Footer </p>
+            <div className="text-center">
+                <p className="text-xl font-bold mt-4 mb-3">Challenges</p>
+                <div className="grid text-center lg:max-w-5xl lg:w-full mt-4 lg:m-0 lg:grid-cols-4 lg:text-left">                
+                    {links.map((link) => (
+                        <Link key={link.name} href={link.href}>
+                            <div className="p-4 bg-[#303b5a7e] rounded-xl hover:bg-[#303b5ab0]">
+                                <p className="text-lg font-bold">{link.name}</p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
